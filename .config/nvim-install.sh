b118798341aebd21176adab31ba8cca6afecb4f6
xnegra80 <xnegra80@gmail.com>
2020-07-13 11:44:49 +0800

    nvim install sh

---
+++ .config/nvim-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+XTRACE=$(set +o | grep xtrace)
+set -o xtrace
+
+echo "Install Neovim"
+brew install nvim
+
+echo "Install yarn"
+brew install yarn
+
+echo "Install Vim-plug as Package Manager"
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Get init.vim"
+cp -rf ~/dotfiles/.config/nvim/ ~/.config/nvim
+cp -rf ~/dotfiles/.config/coc/ ~/.config/coc
+cd ~/.config/coc/extensions/
+yarn install
+
+$XTRACE
